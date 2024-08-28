c02b21ddba20e00fb3c10ec2438e7a59bffec7af
Admin <iwannabeinyouforever@gmail.com>
2024-08-28 07:41:34 +0530

    6th

--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -8,23 +8,27 @@ class ResponsiveButton extends StatelessWidget {
   final double? width;
   final VoidCallback? onPressed; // Add this line
 
-  ResponsiveButton({super.key, this.width=120, this.isResponsive, this.onPressed});
+  ResponsiveButton({super.key, this.width = 120, this.isResponsive, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
-      child: Container(
+      child: InkWell(
+        onTap: onPressed,
         child: Container(
-          width:  isResponsive==true?double.maxFinite:width,
+          width: isResponsive == true ? double.maxFinite : width,
           height: 60,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: AppColors.mainColor,
           ),
           child: Row(
-            mainAxisAlignment:  isResponsive==true?MainAxisAlignment.spaceBetween:MainAxisAlignment.center,
+            mainAxisAlignment: isResponsive == true ? MainAxisAlignment.spaceBetween : MainAxisAlignment.center,
             children: [
-              isResponsive==true?Container(margin:const EdgeInsets.only(left:20),child: AppText(text: "Book Trip Now",color: Colors.white,)):Container(),
+              isResponsive == true ? Container(
+                margin: const EdgeInsets.only(left: 20),
+                child: AppText(text: "Book Trip Now", color: Colors.white),
+              ) : Container(),
               Image.asset("img/button-one.png"),
             ],
           ),
@@ -33,3 +37,4 @@ class ResponsiveButton extends StatelessWidget {
     );
   }
 }
+
